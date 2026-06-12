c257aff0ce7cd7ba465b2b2b67d70f0b723c4187
LeoBjerner-dev <leonard.bjerner@gmail.com>
2026-06-12 09:53:53 +0200

    Fixed navigation in homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,19 @@
+import rooms from "../data/rooms.json";
+import { Link } from "react-router-dom";
 
 
 export const Home = () => {
-    return (
-        <h1>Escape the System</h1>
-    )
-}
-export default Home
+
+  return (
+    <div>
+      <h1>Escape the System</h1>
+      {rooms.map((room) => (
+        <Link to={room.roomPath}>
+            <h3>{room.roomName}</h3>
+        </Link>
+        
+      ))}
+    </div>
+  );
+};
+export default Home;
