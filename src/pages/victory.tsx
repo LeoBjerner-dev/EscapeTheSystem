a66cc95b3905ee171ea98e3f32e20c170bcb0232
joakimwigsten-svg <joakim.wigsten@gmail.com>
2026-06-13 00:16:01 +0200

    Added Victory picture added function to reset game when you cleared the game

--- src/pages/victory.tsx
+++ src/pages/victory.tsx
@@ -1,8 +1,23 @@
-
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { InventoryContext } from "../InventoryProvider"
 
 const Victory = () => {
+    const inventory = useContext(InventoryContext);
+    if (!inventory) return null;
+
     return (
-        <h1>victory</h1>
+        <>
+            <h1>Congratulations!</h1>
+
+            <img src="/images/extra/victory.png" alt="Victory" width={500} />
+
+            <p> You escaped project NEXUS and completed the game.</p>
+
+            <Link to="/">
+                <button onClick={inventory.resetGame}>Play Again</button>
+            </Link>
+        </>
     )
 }
 
